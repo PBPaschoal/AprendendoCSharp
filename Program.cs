1aaf7b6223df1a8722b884e1fe323eea0e488337
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-07-03 00:02:29 -0300

    Vetores 01
    Entrada de dados em C # Teste Vectors
    
    Leia um texto até a quebra de linha e armazene em uma variável.
    Leia três palavras, uma em cada linha, armazenando cada uma em uma variável.
    Leia três palavras na mesma linha, selecione o espaço, armazene uma vez em uma variável.
    ---
    Read a text for the line break and store it in a variable.
    Read three words, one on each line, storing each in a variable.
    Read three words on the same line, select the space, store it once in a variable.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,42 @@
-﻿using System;
+﻿/*Praticando entrada de dados em C#
+ *Na variável 'frase' iremos digitar por exemplo: Bom dia!
+ *Na variável 'x' digitamos por exemplo: Verde
+ *Na variável 'y' digitamos por exemplo: Azul
+ *Na variável 'z' Digitamos por exemplo: Rosa
+ *Por ultumi digitamos 3 palavras juntas (na mesma linha) e separar nas variaveis.
+ *Ler um texto até a quebra de linha e armazenar em uma variável.
+ *Ler três palavras, uma em cada linha, armazenando cada uma em uma variável.
+ *Ler três palavras na mesma linha, separadas por espaço, armazenando cada uma em uma variável.
+ */
 
-namespace EntradaEsaida
+
+using System;
+
+namespace Vetores01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double x, y, media;
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
 
-            Console.WriteLine("Digite o primeiro numero: ");
-            x = double.Parse(Console.ReadLine());
-            Console.WriteLine("Digite o segundo numero: ");
-            y = double.Parse(Console.ReadLine());
-            media = (x + y) / 2.0;
-            Console.WriteLine("Media = " + media);
+            string[] v = Console.ReadLine().Split(' ');
+            string a = v[0];
+            string b = v[1];
+            string c = v[2];
+
+            Console.WriteLine("Você digitou: ");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
         }
     }
 }
