0f51f860ef5406f24cf6c6bceef8aa2366eee52c
Paulo Bruno Paschoal <38387802+PBPaschoal@users.noreply.github.com>
2020-08-09 21:45:17 -0300

    Programa de senhas 01
    Inspirado em filmes, fiz uma brincadeira para praticar.

---
+++ Programming Password/Programming Password/Program.cs
@@ -0,0 +1,44 @@
+﻿//Apenas uma brincadeira que eu fiz para praticar, inspirado em filmes de Hacker
+using System;
+
+namespace Programming_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, contador = 0;
+            double sen;
+
+            sen = 1234;
+            Console.WriteLine("Acesso restrito ");
+            Console.WriteLine("There is no valid permission to access the data.");
+            A = int.Parse(Console.ReadLine());
+            do {
+                if (A == sen) {
+                    Console.WriteLine("Loading...");
+                    Console.WriteLine("Loading... 3%");
+                    Console.WriteLine("Loading... 9%");
+                    Console.WriteLine("Loading... 28%");
+                    Console.WriteLine("Loading... 44%");
+                    Console.WriteLine("Loading... 71%");
+                    Console.WriteLine("Loading... 88%");
+                    Console.WriteLine("Loading... 89%");
+                    Console.WriteLine("Loading... 96%");
+                    Console.WriteLine("Loading... 99%");
+                    Console.WriteLine("Loading... 100%");
+                    Console.WriteLine("\nEntry allowed successfully. Database access available\n");
+                    break;
+                }
+                else {
+                    Console.WriteLine("\nAccess denied\n");
+                    A = int.Parse(Console.ReadLine());
+                }
+                contador++;
+            } while (contador < 2);
+            if (contador == 2) {
+                Console.WriteLine("\nToo many attempts. Connection closed\n");
+            }
+        }
+    }
+}
